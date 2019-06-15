30b3afd9f344390337a563337c10137bee08ceb0
tomczaka <tomczakaneta2@gmail.com>
2019-06-15 12:15:40 +0200

    tiru

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,16 +3,17 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Aneta"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie, output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
